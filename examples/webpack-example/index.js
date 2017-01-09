e4ad40bac47f39154f0662fa37e3f9e41aa401f8
yinhangfeng <darkwander.y@gmail.com>
2017-01-09 22:29:19 +0800

    webpack-example

--- examples/webpack-example/index.js
+++ examples/webpack-example/index.js
@@ -1,4 +1,31 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('root'));
+class App extends React.Component {
+    render() {
+        return (
+            <div ref="root">
+                <h1>Hello React!</h1>
+                <Test1>
+                    <p ref="test_ref">test ref</p>
+                </Test1>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        console.log('App componentDidMount refs:', this.refs);
+    }
+}
+
+class Test1 extends React.Component {
+    render() {
+        return this.props.children;
+    }
+
+    componentDidMount() {
+        console.log('Test1 componentDidMount refs:', this.refs);
+    }
+}
+
+ReactDOM.render(, document.getElementById('root'));
